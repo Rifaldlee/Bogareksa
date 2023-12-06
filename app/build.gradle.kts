f24b55baa419b1bcc89c9a4818a7c14683f94552
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2023-12-06 13:35:04 +0700

    success login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,10 @@ dependencies {
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
+
+
+    /// RETROFIT API ///
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
---
+++ app/src/main/java/com/bogareksa/io/retrofit/ApiService.kt
@@ -0,0 +1,15 @@
+package com.bogareksa.io.retrofit
+
+import com.bogareksa.io.response.ResponseAuth
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.POST
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Query
+
+interface ApiService {
+    @FormUrlEncoded
+    @POST("login")
+    fun postLogin(@Field("email") email: String, @Field("password") password: String):
+            Call<ResponseAuth>
+}
